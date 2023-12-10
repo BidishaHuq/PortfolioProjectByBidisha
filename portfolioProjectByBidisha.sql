c3424be5e2f1ddabd80853c98ec0c2d73e4536c5
Bidisha Huq <148275834+BidishaHuq@users.noreply.github.com>
2023-12-10 19:43:50 +0000

    Add files via upload

--- portfolioProjectByBidisha.sql
+++ portfolioProjectByBidisha.sql
@@ -1,8 +1,6 @@
 Select * from PortfolioProject..CovidDeaths
 order by 3,4
 
---select * from PortfolioProject..CovidVaccinations
---order by 3,4
 
 select Location, date, total_cases, new_cases, total_deaths, population
 from PortfolioProject..CovidDeaths
@@ -20,44 +18,87 @@ order by 1,2
  
 select Location, date, population, total_cases, (total_cases/population)*100 as covidpercentage
 from PortfolioProject..CovidDeaths
-where location like '%bangladesh%'
+-- where continent is not null -- location like '%bangladesh%'
 order by 1,2
 
 
 -- looking at countries with higest infection rate compared to population
-
-select Location, population, Max(total_cases) as HighestInfectionCount, Max((total_cases/population))*100 as percentagePopulationInfected
-from PortfolioProject..CovidDeaths
-group by location, population
+select Location, population, continent,Max(total_cases) as HighestInfectionCount, Max((total_cases/population))*100 as percentagePopulationInfected
+from PortfolioProject..CovidDeaths 
+where continent is not null
+group by location, population, continent
 order by percentagePopulationInfected desc
 
--- showing countries with highest death count per population
+-- creating a view for countries with higest infection rate compared to population
+create view percentagePopulationInfected as 
+select Location, population, continent,Max(total_cases) as HighestInfectionCount, Max((total_cases/population))*100 as percentagePopulationInfected
+from PortfolioProject..CovidDeaths 
+where continent is not null
+group by location, population, continent
 
+
+-- showing countries with highest death count per population
 select Location, MAX(cast(total_deaths as int)) as TotalDeathCount 
 from PortfolioProject..CovidDeaths
 where continent is not null
 group by location
 order by TotalDeathCount desc
 
---lets break things down by continent
- 
+-- creating a view for countries with highest death count per population
+create view TotalDeathCount as
+select Location, MAX(cast(total_deaths as int)) as TotalDeathCount 
+from PortfolioProject..CovidDeaths
+where continent is not null
+group by location
+
+/* 
+	lets break things down by continent
+*/
 
 -- showing continets with the highest death count per population
-
-select continent, MAX(cast(total_deaths as int)) as TotalDeathCount 
+select continent, sum(cast(new_deaths AS int)) as heighest_death
 from PortfolioProject..CovidDeaths
 where continent is not null
 group by continent
-order by TotalDeathCount desc
 
--- golbal numbers
-
-select sum(new_cases) as totalcases, sum(cast(new_deaths as int)) as totaldeaths, sum(cast(new_deaths as int))/sum(new_cases)*100 as deathpercentage
+-- creating a view for continets with the highest death count per population
+create view heighest_death as
+select continent, sum(cast(new_deaths AS int)) as heighest_death
 from PortfolioProject..CovidDeaths
 where continent is not null
---group by date
+group by continent
+
+/* 
+	golbal numbers
+*/
+
+-- total cases, deaths and percentage in the entire world 
+select sum(new_cases) as totalCases, sum(cast(new_deaths as int)) as totalDeaths, 
+sum(cast(new_deaths as int))/sum(new_cases)*100 as deathPercentage
+from PortfolioProject..CovidDeaths
+where continent is not null
 order by 1,2
 
+--total new cases everyday in the world
+select date, sum(cast(new_cases as int)) as total_new_cases_in_the_world 
+from PortfolioProject..CovidDeaths 
+where continent is not null 
+group by date 
+order by 1, 2
+
+-- new cases, new deaths and death percentages per day
+select date, sum(new_cases) as new_cases, SUM(cast(new_deaths as int)) new_deaths, 
+SUM(convert(int, new_deaths))/ sum(new_cases)*100 as deathPerCasesPercent 
+from CovidDeaths where continent is not null
+group by date 
+order by 4 desc
+
+--creating view for deathPerCasesPercent per day in the entire world
+create view deathPerCasesPercent as
+select date, sum(new_cases) as new_cases, SUM(cast(new_deaths as int)) new_deaths, 
+SUM(convert(int, new_deaths))/ sum(new_cases)*100 as deathPerCasesPercent 
+from CovidDeaths where continent is not null
+group by date
 
 --- looking at toal population vs vaccinations
 
